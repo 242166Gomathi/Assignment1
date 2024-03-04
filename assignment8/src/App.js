0ffd732f67dd83c33a3bb405f31984e9a0c3e2b4
242166Gomathi <160576855+242166Gomathi@users.noreply.github.com>
2024-03-04 12:04:26 +0530

    Add files via upload
    React Lifecycle

---
+++ assignment8/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+import LifeCycle, { Demo } from './component/lifecycle.js';
+
+function App() {
+  return (
+    <div className="App">
+    <Demo />
+      <LifeCycle />
+    </div>
+  );
+}
+
+export default App;
